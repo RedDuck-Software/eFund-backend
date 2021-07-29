02e32515445160ff17247b3f264b2c603a0121c0
kostyamospan <kostya04042001@gmail.com>
2021-07-29 14:02:07 +0300

    addresses validation

--- EFund.Domain/Models/Request/RegisterUserRequest.cs
+++ EFund.Domain/Models/Request/RegisterUserRequest.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EFund.Domain.Models.Request
 {
-    public class RegisterUserRequest : RequestBase
+    public class RegisterUserRequest
     {
+        [RegularExpression("^0x[a-fA-F0-9]{40}$")]
         public string Address { get; set; }
         
         public string SignedGenericNonce { get; set; }
--- EFund.Domain/Models/Request/UpdateUserInfo.cs
+++ EFund.Domain/Models/Request/UpdateUserInfo.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EFund.Domain.Models.Request
 {
-    public class UpdateUserInfoRequest : RequestBase
+    public class UpdateUserInfoRequest
     {
+        [RegularExpression("^0x[a-fA-F0-9]{40}$")]
         public string Address { get; set; }
 
         public string Username { get; set; }
