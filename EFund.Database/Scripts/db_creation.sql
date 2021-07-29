d3526711ab20f18a5c0c1bc9b7f1749cd9fb6715
kostyamospan <kostya04042001@gmail.com>
2021-07-29 14:01:00 +0300

    db script updated

--- EFund.Database/Scripts/db_creation.sql
+++ EFund.Database/Scripts/db_creation.sql
@@ -1,15 +1,14 @@
 CREATE DATABASE IF NOT EXISTS [efund];
 USE [efund];
 
-
-CREATE TABLE [network] ( 
-  [ChainId] SMALLINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+CREATE TABLE [networks] ( 
+  [ChainId] SMALLINT NOT NULL PRIMARY KEY,
   [PlatformContractAddress] BINARY(20) NOT NULL 
 )
 
 CREATE TABLE [hedgefund_infos] (
-  [ContractId] BINARY(20) NOT NULL PRIMARY KEY,
-  [ChainId] SMALLINT NOT NULL FOREIGN KEY REFERENCES network([ChainId]),
+  [ContractAddress] BINARY(20) NOT NULL PRIMARY KEY,
+  [ChainId] SMALLINT NOT NULL FOREIGN KEY REFERENCES networks([ChainId]),
   [Name] VARCHAR(255) NOT NULL,
   [Description] VARCHAR(255) NULL, 
   [ImageUrl] VARCHAR(100) NOT NULL, 
@@ -17,7 +16,7 @@ CREATE TABLE [hedgefund_infos] (
 
 create Table [users] (
 	[Address] BINARY(20) NOT NULL PRIMARY KEY, 
-	[ChainId] SMALLINT NOT NULL FOREIGN KEY REFERENCES network([ChainId]),
+	[ChainId] SMALLINT NOT NULL FOREIGN KEY REFERENCES networks([ChainId]),
 	[SignNonce] VARCHAR(40) NOT NULL,
 	[Username] VARCHAR(255) NULL,
 	[ImageUrl] VARCHAR(100) NULL, 
